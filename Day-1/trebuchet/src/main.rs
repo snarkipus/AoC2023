74e9325dca7365071253feade0224f454f4e0bc2
Matt Jackson <matt@jacksonsix.com>
2023-12-03 18:31:19 -0500

    day 1 - part 2 complete

--- Day-1/trebuchet/src/main.rs
+++ Day-1/trebuchet/src/main.rs
@@ -1,8 +1,11 @@
 use color_eyre::eyre::Result;
+use std::collections::HashMap;
+use std::error::Error;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::str::FromStr;
+use tracing::debug;
 use tracing::info;
 use tracing_subscriber::{filter::targets::Targets, layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -18,10 +21,7 @@ fn main() {
     let data = parse_data(input).unwrap();
 
     // 3) Process data
-    let mut total = 0;
-    data.iter().for_each(|number| {
-        total += number;
-    });
+    let total = data.iter().sum::<usize>();
 
     // 4) Print result
     println!("Total: {}", total);
@@ -42,17 +42,69 @@ fn read_input(filename: &str) -> Result<Vec<String>> {
     Ok(result)
 }
 
+#[tracing::instrument]
 fn parse_data(data: Vec<String>) -> Result<Vec<usize>> {
     let mut result: Vec<usize> = Vec::new();
     data.iter().for_each(|line| {
-        let first_digit = line.chars().find(|c| c.is_ascii_digit()).unwrap();
-        let last_digit = line.chars().rev().find(|c| c.is_ascii_digit()).unwrap();
+        let new_line = replace_strings(line);
+        let first_digit = new_line.chars().find(|c| c.is_ascii_digit()).unwrap();
+        let last_digit = new_line.chars().rev().find(|c| c.is_ascii_digit()).unwrap();
         // concatenate first and last digits as a usize
         let number = format!("{}{}", first_digit, last_digit).parse::<usize>();
         result.push(number.unwrap());
     });
 
     Ok(result)
+}
+
+#[tracing::instrument]
+fn replace_strings(line: &str) -> String {
+    let mut result = String::new();
+
+    let map = HashMap::from([
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+
+    let mut i = 0;
+
+    while i < line.len() {
+        let _foo = &line[i..];
+
+        // check if the current character is a digit - if so, add it to the result
+        if line.chars().nth(i).unwrap().is_ascii_digit() {
+            result.push(line.chars().nth(i).unwrap());
+            i += 1;
+            continue;
+        }
+
+        // check if the current character is the beginning of a string to be replaced
+        // if so, replace it and move the index forward to the last character of the replacement
+        let mut replaced = false;
+        for (key, &value) in map.iter() {
+            if line[i..].starts_with(key) {
+                result.push_str(value);
+                i += key.len() - 1;
+                replaced = true;
+                break;
+            }
+        }
+
+        // if the current character is not a digit and not the beginning of a string to be replaced
+        // move the index forward by one
+        if !replaced {
+            i += 1;
+        }
+    }
+
+    result
 }
 
 fn install_tracing(level: &str) {
@@ -68,6 +120,7 @@ fn install_tracing(level: &str) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
     #[test]
     fn it_works() {
@@ -113,5 +166,36 @@ mod tests {
     // treb7uchet
     fn test_main() {
         main();
+    }
+
+    #[test_case("two1nine", "219" ; "two1nine")]
+    #[test_case("eightwothree", "823" ; "eightwothree")]
+    #[test_case("abcone2threexyz", "123" ; "abcone2threexyz")]
+    #[test_case("xtwone3four", "2134" ; "xtwone3four")]
+    #[test_case("4nineeightseven2", "49872" ; "4nineeightseven2")]
+    #[test_case("zoneight234", "18234" ; "zoneight234")]
+    #[test_case("7pqrstsixteen", "76" ; "7pqrstsixteen")]
+    // f47ninexfqsbdrseventwo7twonep - overlapping string case from the data - super sucked
+    #[test_case("f47ninexfqsbdrseventwo7twonep", "47972721" ; "f47ninexfqsbdrseventwo7twonep")]
+    fn test_replace_strings(input: &str, expected: &str) {
+        let mut result = Vec::<String>::new();
+        let line = String::from(input);
+        result.push(replace_strings(&line));
+        assert_eq!(result[0], expected);
+    }
+
+    #[test]
+    fn test_parse_data_2() {
+        let data = read_input("../test-2.txt").unwrap();
+        let result = parse_data(data).unwrap();
+        assert_eq!(result.len(), 7);
+        assert_eq!(result[0], 29);
+        assert_eq!(result[1], 83);
+        assert_eq!(result[2], 13);
+        assert_eq!(result[3], 24);
+        assert_eq!(result[4], 42);
+        assert_eq!(result[5], 14);
+        assert_eq!(result[6], 76);
+        assert_eq!(result.iter().sum::<usize>(), 281);
     }
 }
